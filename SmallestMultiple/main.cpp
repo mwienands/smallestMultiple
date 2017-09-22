703454edae48f546dce9696db36066398954bf48
mwienands <mwienands@gmail.com>
2017-09-22 14:23:19 -0500

    Initial Commit

---
+++ SmallestMultiple/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SmallestMultiple
+//
+//  Created by Michael Wienands on 9/22/17.
+//  Copyright © 2017 Michael Wienands. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
